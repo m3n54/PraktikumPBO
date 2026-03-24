93573b83ed585a45502d0fa36a3a81c90f62e777
menza isaiah <menzaisaiah1@gmail.com>
2026-03-24 13:59:49 +0700

    Pertemuan6

---
+++ Pertemuan6/src/AngkaSialException.java
@@ -0,0 +1,11 @@
+/* Nama File    : AngkaSialException.java
+ * Deskripsi    : Eksepsi buatan sendiri, diturunkan dari Exception
+ * Pembuat      : Menza Isaiah Tampubolon
+ * Tanggal      : 24 Maret 2026
+ */
+
+class AngkaSialException extends Exception {
+    public AngkaSialException() {
+        super("Angka sial detected!");
+    }
+}
